880c3c83b4ee59bdce6fa2495142ae04a3ecb389
Oscar Maestro <oscar_maestro@epam.com>
2022-07-11 16:21:18 -0500

    Adding basic operations

--- base.java
+++ base.java
@@ -3,4 +3,20 @@ public class base{
 	public int add(int a, int b){
 		return a + b;
 	}
+	
+	public int sub(int a, int b){
+		return a - b;
+	}
+	
+	public double div(int a, int b){
+		return a/b;
+	}
+	
+	public int mult(itn a, int b){
+		return a * b;
+	}
+	
+	public double sqrt(int a, int b){
+		return Math.sqrt(a, b);
+	}
 }
